ee357be42b2590bf433f478b4d979b419d7a0b83
ignaciogarcia <ig@pep.security>
2024-05-06 07:12:04 +0200

    Add models and migrations

--- TaskManagerApp/models.py
+++ TaskManagerApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200, default="")
+    text = models.CharField(max_length=500, default="")
+    creation_date = models.DateTimeField("creation date", default=timezone.now)
+    due_date = models.DateTimeField("due date", null=True)
+    is_done = models.BooleanField(default=False)
+    def __str__(self):
+        return """
+task title: {}
+task text: {}
+is done: {}
+""".format(self.title, self.text, self.is_done)
