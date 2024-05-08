2eb6931828a77aec1b566be63fe8488d6774bccb
ignaciogarcia <ig@pep.security>
2024-05-08 05:50:23 +0200

    Add templates and use patterns for url flexibility

--- MyTaskManager/urls.py
+++ MyTaskManager/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("tasks/", include("TaskManagerApp.urls")),
+    path("task-manager/", include("TaskManagerApp.urls")),
 ]
--- TaskManagerApp/views.py
+++ TaskManagerApp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from .models import Task
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+def all_tasks(request):
+    task_list = Task.objects.order_by("due_date")
+    context = { "task_list": task_list } if task_list.exists() else {}
+    return render(request, "TaskManagerApp/all_tasks.html", context)
+
+def task_detail(request, task_id):
+    #return HttpResponse("You're looking at task %s." % task_id)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, "TaskManagerApp/task_detail.html", {"task": task})
